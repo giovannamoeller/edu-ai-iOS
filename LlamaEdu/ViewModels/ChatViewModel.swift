5fda17522758dc445172a81f0d69da5681f391ad
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-08 08:49:39 -0300

    create chat view model

---
+++ LlamaEdu/ViewModels/ChatViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ChatViewModel.swift
+//  LlamaEdu
+//
+//  Created by Giovanna Moeller on 08/11/24.
+//
+
+import Foundation
+
+@MainActor
+class ChatViewModel: ObservableObject {
+    @Published var messages: [Message] = []
+    @Published var isLoading: Bool = false
+    
+    func loadPreviousMessages(for subject: Subject) async throws {
+        //
+    }
+    
+    func sendMessage(_ text: String, subject: Subject) async {
+        let message = Message(id: UUID(), text: text, type: .human, timestamp: Date())
+        messages.append(message)
+        
+    }
+}
