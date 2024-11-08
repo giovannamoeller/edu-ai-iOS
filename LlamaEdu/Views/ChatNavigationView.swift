b5d7ae4f8599aca46f416b11c428d2b317804563
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-08 00:07:20 -0300

    add subject enum

--- LlamaEdu/Views/ChatNavigationView.swift
+++ LlamaEdu/Views/ChatNavigationView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+enum Subject: String, CaseIterable {
+    case matematica = "Matemática"
+    case portugues = "Português"
+    case historia = "História"
+    case geografia = "Geografia"
+    case biologia = "Biologia"
+    case quimica = "Química"
+    case fisica = "Física"
+}
+
 struct ChatNavigationView: View {
+    let subjects = Subject.allCases
+    @State private var selectedSubject = ""
+    
     var body: some View {
-        Text("Chat navigation")
+        Text(selectedSubject)
+            .onAppear {
+                self.selectedSubject = subjects.first!.rawValue
+            }
     }
 }
 
